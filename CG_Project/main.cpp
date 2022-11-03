e0ccbe64412c3b02b4f486b1023725bc6107b980
Famelgas <filipe.damendes@gmail.com>
2022-11-03 23:11:17 +0000

    Initial Commit

---
+++ CG_Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CG_Project
+//
+//  Created by Filipe on 03/11/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
